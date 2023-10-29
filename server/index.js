5f0b5bf0b62c10cbfc1070f16e13d42048f4e6df
achaltelmasre <achaltelmasre@gmail.com>
2023-10-29 10:57:20 +0530

    create get , patch, request for order

--- server/index.js
+++ server/index.js
@@ -225,13 +225,13 @@ app.post("/order", async (req, res) => {
 app.get("/order/:id", async (req, res) =>{
     const {id} = req.params;
 
-    const order = await Order.findById(id).populate("user");
+    const order = await Order.findById(id).populate("user product");
 
     order.user.password = undefined;
-    order.user.gender = undefined;
-    order.user.address = undefined;
-    order.user.createdAt = undefined;
-    order.user.updatedAt = undefined;
+    // order.user.gender = undefined;
+    // order.user.address = undefined;
+    // order.user.createdAt = undefined;
+    // order.user.updatedAt = undefined;
 
     res.json({
       success: true,
@@ -240,10 +240,48 @@ app.get("/order/:id", async (req, res) =>{
     });
 })
 
-//Get/order/user/:id
+//Get/orders/user/:id
+app.get("/orders/user/:id", async(req, res) =>{
+    const {id} = req.params;
 
+    const orders = await Order.find({user: id}).populate("user product");
+    
+    res.json({
+        success: true,
+        data: orders,
+        message: "Order fetched successfully"
+    });
+});
 
-//Patch/order/:id
+//Patch/order/status/:id
+app.patch("/order/status/:id", async(req, res) => {
+ const {id} = req.params;
+
+ const {status} = req.body;
+
+ await Order.updateOne ({_id: id},
+    {$set: {status:status}});
+
+    const updatedProduct = await Order.findOne({_id: id})
+
+    res.json({
+        success: true,
+        data: updatedProduct,
+        message:" Order status updated successfully"
+    });
+    
+});
+
+//Get /orders
+app.get("/orders", async(req, res) => {
+  const orders = await Order.find();
+
+  res.json({
+    success: true,
+    data:orders,
+    message: "Orders fetched successfully"
+  });
+   });
 
 const PORT = process.env.PORT || 5000;
 
