f0638f4b4495ab752d32285697626af21f7f528d
achaltelmasre <achaltelmasre@gmail.com>
2023-10-30 22:54:15 +0530

    design signup form

--- client/src/view/Signup/Signup.js
+++ client/src/view/Signup/Signup.js
@@ -6,9 +6,10 @@ function Signup() {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');    const [mobile, setMobile] = useState('');
-    const {address, setAddress} = useState('');
-    const {gender, setGender} = useState('');
+    const [password, setPassword] = useState('');   
+    const [mobile, setMobile] = useState('');
+    const [address, setAddress] = useState('');
+    const [gender, setGender] = useState('female');
 
     
     const signup = async () => {
@@ -19,50 +20,96 @@ function Signup() {
            return 
         }
      
-        const response = await axios.post(
-            {
+        const response = await axios.post("/signup", {
+              
                 name:name,
                 email:email,
                 password:password,
                 mobile:mobile,
                 address:address,
                 gender:gender
+            })
 
+            alert(response?.data?.message);
+
+            if (response?.data?.success) {
+                alert(response?.data?.message);
+                window.location.href = "/login";
             }
-        )
-
-    }
-  
-    
+    };
+ 
     return(
-      <div>
-           <h2> Signup</h2>
+      <div className="signup">
+        <form className="main-container">
+           <h1 className="text-center"> Signup</h1>
 
            <div>
             <lable htmlFor='name'>Name:</lable>
             <input type='text' placeholder="Enter your name" id='name' value={name}
+            className="input-form"
+             onChange={(e) => {setName(e.target.value)}}
             />
            </div>
 
            <div>
             <lable htmlFor='email'>Email:</lable>
-            <input type='email' placeholder="Enter your email" id='email' value={email}/>
+            <input type='email' 
+              placeholder="Enter your email" 
+              id='email' value={email}
+              className="input-form"
+              onChange={(e) => {setEmail(e.target.value)}}/>
            </div>
 
            <div>
             <lable htmlFor='password'>Password:</lable>
-            <input type='password' placeholder="Enter your password" id='password' value={password}/>
+            <input type='password' 
+              placeholder="Enter your password" 
+              id='password' value={password}
+              className="input-form"
+              onChange={(e) => {setPassword(e.target.value)}}/>
            </div>
 
            <div>
             <lable htmlFor='mobile'>MobileNo.:</lable>
-            <input type='text' placeholder="Enter your mobile number" id='mobile' value={mobile}/>
+            <input type='text' 
+              placeholder="Enter your mobile number"
+              id='mobile' value={mobile}
+              className="input-form"
+              onChange={(e) => {setMobile(e.target.value)}}/>
            </div>
 
            <div>
             <lable htmlFor='address'>Address:</lable>
-            <input type='text' placeholder="Enter your address" id='address' value={address}/>
+            <input type='text'
+              placeholder="Enter your address"
+              id='address' value={address}
+              className="input-form"
+              onChange={(e) => {setAddress(e.target.value)}}/>
            </div>
+
+           <div>
+            
+            <input type="radio"
+                 name="gender"
+                  id="male" 
+                  className="gender" 
+                  checked={gender === "male"} 
+                  onClick={(e) => { setGender("male")}}/>
+            <label htmlFor="male">Male </label>
+
+            <input type="radio" name="gender"     
+                   id="female" 
+                   className="gender" 
+                   checked={gender === "female"}
+                   onClick={(e) => { setGender("male")}}/>
+            <label htmlFor="female">Female </label>
+           </div>
+
+           <button type="button" className="btn signup-btn"
+            onClick={signup}>
+             Signup
+           </button>
+         </form>
       </div>
      
     
